a4a3fad4076cc0ce9f7060e0b67c89ed1c053d0d
Arnaud Moncel <arnaud.moncel43@gmail.com>
2022-09-12 17:03:47 +0200

    chore: prepare release

---
+++ .releaserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  branches: [ 'main' ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          {
+            scope: 'force-release',
+            release: 'patch',
+          },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ],
+};
